521965bb492e689abb4e8bb95cd34141a037d150
masato.matsuda <masato220@gmail.com>
2018-11-21 15:37:18 +0000

    favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relastionships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relastionships, source: :user
+  has_many :favorites, dependent: :destroy # dependentを追記することで関連付くイベントが削除できるようになる
+  has_many :fav_microposts, through: :favorites, source: :micropost
+  
   
   def follow(other_user)
     unless self == other_user
@@ -32,4 +35,12 @@ class User < ApplicationRecord
     Micropost.where(user_id: self.following_ids + [self.id])
   end
   
+  def like(micropost)
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unlike(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
 end
